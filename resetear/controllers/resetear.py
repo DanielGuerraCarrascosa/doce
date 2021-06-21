9c7a7592d86e9b9303d35747f54cb89d9fc5a156
DanielGuerraCarrascosa <d.guerra@ampsoftware.com>
2021-06-21 11:57:16 +0000

    --

--- resetear/controllers/resetear.py
+++ resetear/controllers/resetear.py
@@ -1,63 +1,60 @@
 # -*- coding: utf-8 -*-
-
 import logging
-
-from ast import literal_eval
-from collections import defaultdict
-from dateutil.relativedelta import relativedelta
-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-from odoo.osv import expression
-from odoo.tools.misc import ustr
-
-from odoo.addons.base.models.ir_mail_server import MailDeliveryException
-from odoo.addons.auth_signup.models.res_partner import SignupError, now
-
-_logger = logging.getLogger(__name__)
-
 from odoo import http
 from odoo.http import request
+from odoo.exceptions import UserError
+ 
 
-class Resetear(http.Controller):
-    
-    """_inerit = 'ResUsers'"""
-    _inherit = 'res.users'
-    
-    @http.route('/resetear', type='http', csrf=False, auth='public', website=True)
-    def resetear(self, *args, **kw):
-        db = 'danielguerracarrascosa-doce-caso-1-2735523'
-        login = 'd.guerra@ampsoftware.com'
-        password = '12345m'
+_logger = logging.getLogger(__name__)
+ 
+
+class ChangePasswdExternal(http.Controller):
+    _inerit = 'res.users'
+    @http.route('/reseteo', auth='none', website=True)
+    def reset_password(self, **kw):
+        db = 'danielguerracarrascosa-doce'
+        login = 'v.fernandez@ampsoftware.com'
+        password = 'toor1'
         request.session.authenticate(db, login, password)
-        
         emailRecibido = request.params['email']
-        
-        user = http.request.env['res.users'].sudo().search([('email', '=', emailRecibido)])
-        
-        
+        user = http.request.env['res.users'].search([('email', '=', emailRecibido)])
+        print("hola estoy en la funcion reset_password por i1nherit")
+        print(user)
+        print(type(user))
+        if len(user) != 1:
+            return "no existe el correo introducido"
+        else:
+            print("usuario existente en BDD")
+            return user.action_reset_password()
+ 
+
+ 
+
+    def action_reset_password(self):
         """ create signup token for each user, and send their signup url by email """
-        if http.request.env.context.get('install_mode', False):
-            return
-        
         # prepare reset password signup
-        create_mode = bool(http.request.env.context.get('create_user'))
+        print("Llego a la funcion action_reset_password")
+        create_mode = bool(self.env.context.get('create_user'))
+ 
 
         # no time limit for initial invitation, only for reset password
         expiration = False if create_mode else now(days=+1)
+ 
+
+        self.mapped('partner_id').signup_prepare(signup_type="reset", expiration=expiration)
+ 
 
         # send email to users with their signup url
         template = False
-        
         if create_mode:
             try:
-                template = http.request.env.ref('auth_signup.set_password_email', raise_if_not_found=False)
+                template = self.env.ref('auth_signup.set_password_email', raise_if_not_found=False)
             except ValueError:
                 pass
         if not template:
-            template = http.request.env.ref('auth_signup.reset_password_email')
-            
+            template = self.env.ref('auth_signup.reset_password_email')
         assert template._name == 'mail.template'
+ 
 
         template_values = {
             'email_to': '${object.email|safe}',
@@ -66,9 +63,12 @@ class Resetear(http.Controller):
             'partner_to': False,
             'scheduled_date': False,
         }
-        template.sudo().write(template_values)
+        template.write(template_values)
+ 
 
-        force_send = not(http.request.env.context.get('import_file', False))
-        template.send_mail(user.id, force_send=force_send, raise_exception=True)
-        
-        return 'ok'
+        for user in self:
+            if not user.email:
+                raise UserError(_("Cannot send email: user %s has no email address.") % user.name)
+            with self.env.cr.savepoint():
+                template.with_context(lang=user.lang).send_mail(user.id, force_send=True, raise_exception=True)
+            _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
