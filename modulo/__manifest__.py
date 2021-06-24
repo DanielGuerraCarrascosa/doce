c04778560343618ad4b9e5d24096bed7616d7fe0
DanielGuerraCarrascosa <d.guerra@ampsoftware.com>
2021-06-24 12:39:51 +0000

    --

---
+++ modulo/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Modulo vacio',
+    
+    'sumary': """ -- """,
+    
+    'description': """ -- """,
+    
+    'author': 'Dani AMP Software',
+    
+    'Website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': [
+        'base',
+    ],
+    
+    'data': [
+    ],
+    
+    'demo': [
+    ],
+}
