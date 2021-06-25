73b32146148ffdc4a49213527237b9f7027dcdba
DanielGuerraCarrascosa <d.guerra@ampsoftware.com>
2021-06-25 07:29:47 +0000

    otro

---
+++ figura/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'otro',
+    
+    'sumary': """ elimino modulo y creo este """,
+    
+    'description': """ bbb """,
+    
+    'author': 'Dani AMP Software',
+    
+    'Website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': [
+        'base',
+    ],
+    
+    'data': [
+    ],
+    
+    'demo': [
+    ],
+}
